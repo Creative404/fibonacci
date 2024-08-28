67dd090ec75f3c355f7a95d8a183a5bfa11965d8
Daniel <daniel.fornalczyk.kontakt@gmail.com>
2024-08-28 03:11:36 +0200

    it start doing something!
    i just add a bunch of garbage ant its star to calculate

--- fibonacci.py
+++ fibonacci.py
@@ -19,14 +19,22 @@ while True:
 while True:
     try:
         ammount_piervus = int(input("how many of piervus numbers u want?: "))
-        break
+        if ammount_piervus > 5 or ammount_piervus < 1:
+            print("have mercy pick something between 1-5")
+            continue
+        else:
+            break
     except ValueError:
         print("Please enter a valid number.")
 
 while True:
     try:
         ammount_next = int(input("how many of next numbers u want?: "))
-        break
+        if ammount_next > 5 or ammount_next < 1:
+            print("have mercy pick something between 1-5")
+            continue
+        else:
+            break
     except ValueError:
         print("Please enter a valid number.")
 
@@ -49,15 +57,41 @@ def looking_for_piervus(fibonacci_start_number):
         #print(fibonacci_piervus_number)
     #idk what im doing
 
-if ammount_piervus >= 1:
-    piervus =  looking_for_piervus(fibonacci_start_number)
-    piervus_list.append(piervus)
-
-print(piervus_list)
 
 
+#massife sitty if's
+piervus =  looking_for_piervus(fibonacci_start_number)
+if piervus >= 0 and ammount_piervus >= 1:
+    piervus_list.append(piervus) #this cant be bigger than this
+    print(piervus_list)
+    ammount_piervus -=1
 
+piervus1 =  looking_for_piervus(piervus)
+if piervus1 >= 0 and ammount_piervus >= 1:
+    piervus_list.append(piervus1)
+    print(piervus_list)
+    ammount_piervus -=1
 
+piervus2 =  looking_for_piervus(piervus1)
+if piervus1 >= 0 and ammount_piervus >= 1:
+    piervus_list.append(piervus2)
+    print(piervus_list)
+    ammount_piervus -=1
+
+piervus3 =  looking_for_piervus(piervus2)
+if piervus1 >= 0 and ammount_piervus >= 1:
+    piervus_list.append(piervus3)
+    print(piervus_list)
+    ammount_piervus -=1
+
+piervus4 =  looking_for_piervus(piervus3)
+if piervus1 >= 0 and ammount_piervus >= 1:
+    piervus_list.append(piervus4)
+    print(piervus_list)
+    ammount_piervus -=1
+
+#remeber add pop ad put it in order and in if's
+#pop remove last - (its for me dot read this shi-)
 
 
 
